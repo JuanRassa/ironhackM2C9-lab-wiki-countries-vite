c61002c4ed0eec2da81c0c5821b0bfca22e7a64c
Juan Rassa <jmrassa.dev@gmail.com>
2023-12-06 20:02:57 +0000

    Complete -> Iteration 2: Setup React Router and Create Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Navbar from './components/Navbar';
 import CountryDetailsPage from './pages/CountryDetailsPage';
 import HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <CountryDetailsPage />
       <h1>LAB | React WikiCountries</h1>
-      <HomePage></HomePage>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/:countryId' element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
