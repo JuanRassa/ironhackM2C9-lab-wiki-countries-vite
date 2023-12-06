232f1ed4a6b550cc07debc6cf6d8a2b7f35c654d
Juan Rassa <jmrassa.dev@gmail.com>
2023-12-06 20:52:18 +0000

    Complete -> Iteration 3: Fetch Countries Data and Display as a List.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,55 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import worldGifLoader from './../assets/earth-gif-preloader.gif';
+
 const HomePage = () => {
+  const [countriesData, setCountriesData] = useState(null);
+
+  useEffect(() => {
+    axios.get(' https://ih-countries-api.herokuapp.com/countries').then(res => {
+      setCountriesData(res.data);
+      console.log('data', res.data);
+    });
+  }, []);
+  console.log('countriesData', countriesData);
   return (
     <div className='container' style={{ maxHeight: '90vh', overflow: 'scroll' }}>
       <h1 style={{ fontSize: '24px' }}>WikiCountries: Your Guide to the World</h1>
-
-      <div className='list-group'>
-        <a className='list-group-item list-group-item-action' href='/ABW'>
-          🇦🇼 Aruba
-        </a>
-        <a className='list-group-item list-group-item-action' href='/AFG'>
-          🇦🇫 Afghanistan
-        </a>
-        <a className='list-group-item list-group-item-action' href='/AGO'>
-          🇦🇴 Angola
-        </a>
-        <a className='list-group-item list-group-item-action' href='/AIA'>
-          🇦🇮 Anguilla
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ALA'>
-          🇦🇽 Åland Islands
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ALB'>
-          🇦🇱 Albania
-        </a>
-        <a className='list-group-item list-group-item-action' href='/AND'>
-          🇦🇩 Andorra
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ARE'>
-          🇦🇪 United Arab Emirates
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ARG'>
-          🇦🇷 Argentina
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ARM'>
-          🇦🇲 Armenia
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ASM'>
-          🇦🇸 American Samoa
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ATA'>
-          🇦🇶 Antarctica
-        </a>
-        <a className='list-group-item list-group-item-action' href='/FLK'>
-          🇫🇰 Falkland Islands
-        </a>
-        <a className='list-group-item list-group-item-action' href='/FRA'>
-          🇫🇷 France
-        </a>
-        <a className='list-group-item list-group-item-action' href='/ZWE'>
-          🇿🇼 Zimbabwe
-        </a>
-      </div>
+      {!countriesData && <img src={worldGifLoader} />}
+      {countriesData && (
+        <div className='list-group'>
+          {countriesData.map(country => (
+            <a key={country._id} className='list-group-item list-group-item-action' href={country.alpha3Code}>
+              {country.name.official}
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
